caaa99e6044a37708fd3f74e85aa52e374b433ea
rashedadwan99 <rashedadwan56789@gmail.com>
2025-04-12 18:57:11 +0300

    sdsd

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 require("./startup/cors")(app);
 require("./startup/db")();
 require("./startup/routes")(app);
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
