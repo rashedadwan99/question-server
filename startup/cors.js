a0bb27857fea948c97ec4a2ce0481bbf6ae08a00
rashedadwan99 <rashedadwan56789@gmail.com>
2025-05-09 21:51:25 +0300

    sfsf

--- startup/cors.js
+++ startup/cors.js
@@ -1,4 +1,12 @@
 const cors = require("cors");
 module.exports = (app) => {
-  app.use(cors());
+  app.use(
+    cors({
+      origin: [
+        "http://localhost:3000",
+        "https://question-client.onrender.com/",
+        "https://q-dashboard.onrender.com/dashboard",
+      ],
+    })
+  );
 };
